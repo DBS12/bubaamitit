9f4b223f1dac437782ffb7eb1abe7deb79b6b245
Ofek <ofekel@ac.sce.ac.il>
2021-09-04 22:51:18 +0300

    hehe

--- theforgottenserver-v1.2-win64/data/creaturescripts/scripts/ScriptLogout.lua
+++
@@ -1,8 +0,0 @@
-function onLogout(player)
-local pid = player:getId()
-    if savePos[pid] then
-        Item(Tile(savePos[pid][2]):getGround().uid):removeAttribute('aid')
-        savePos[pid] = nil
-    end
-    return true
-end
